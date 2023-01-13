1de3064bcb6e1603a9554e3e0cb3045d1c5322b7
mayi-mama <mayimuro8845@gmail.com>
2023-01-13 10:55:48 +0900

    HTML,CSS,jQuery修正

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(function(){
+    /*id=js-pagetopをpagetopに代入*/
     var pagetop = $('#js-pagetop');
+    /*pagetopがページトップから500px離れるとpagetopがでてくる*/
     pagetop.hide();
     $(window).scroll(function(){
         if($(this).scrollTop() > 500){
@@ -8,12 +10,23 @@ $(function(){
             pagetop.fadeOut();
         }
     });
+    /*pagetopをクリックするとページトップまで戻る*/
     pagetop.click(function(){
-        $('body', 'html').animate({
+        $('body, html').animate({
             scrollTop: 0
         }, 500);
         return false;
 
     });
+
+    /*スムーススクロール*/
+    $('a[href^="#"]').click(function () {
+        const speed = 600;
+        let href = $(this).attr("href");
+        let target = $(href == "#" || href == "" ? "html" : href);
+        let position = target.offset().top;
+        $("body,html").animate({ scrollTop: position }, speed, "swing");
+        return false;
+      });
 
 });
